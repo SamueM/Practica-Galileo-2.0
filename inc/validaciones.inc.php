34b5d46a972207788accdf816c0780e8207146a7
Isabel Laura <isabellauraloz@gmail.com>
2017-02-21 16:09:42 +0100

    Nuevas validaciones para modificar un usuario

--- inc/validaciones.inc.php
+++ inc/validaciones.inc.php
@@ -32,18 +32,28 @@ function esFechaNac($fecha){
  * @param type $fecha está en aaaa-mm-dd
  * @return string
  */
-function getFechaNac($fecha){
+function getFechaNac($fechaBD){
    // echo "La fecha del objeto-> ".$fecha;
-    if($fecha!=NULL){
-    $partes=  explode("-", $fecha);
-    $fechaNac=$partes[2]."-".$partes[1]."-".$partes[0];
-        if($fechaNac=='00-00-0000'){
-        $fechaNac= "00/00/0000";
-        }
+    if($fechaBD!=NULL){
+    $partes=  explode("-", $fechaBD);
+    $fechaNac=$partes[2]."/".$partes[1]."/".$partes[0];
+
     }else{
-        $fechaNac= "00/00/0000";
+        $fechaNac="00/00/0000";
     }
-    return $fechaNac;
+        return $fechaNac;
+}
+/**
+ * La contraseña debe tener al entre 4 y 8 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.
+ * NO puede tener otros símbolos.
+ * @param type $pass
+ */
+function esContraseña($pass){
+    if(preg_match("/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{4,8}$/",$pass)){
+        return true;
+    }else{
+        return false;
+    }
 }
 /**
  * 
@@ -70,17 +80,83 @@ function validacionExisteUsuario($num){
         return $mensaje[USER_NOEXIS];  
     }elseif ($num==-305) {
         return $mensaje[PASS_INCORRECTO];
+    }elseif ($num==-204) {
+        return $mensaje[EMAIL_REPE];
     }elseif ($num==-205) {
         return $mensaje[ERROR_FECHA_NACIMIENTO];
-    }   
+    }elseif ($num==-206) {
+        return $mensaje[TFNO_INCORRECTO];
+    }elseif ($num==-207) {
+        return $mensaje[NOMBRE_INCORRECTO];
+    }elseif ($num==-208) {
+        return $mensaje[APELLIDO_INCORRECTO];
+    }elseif ($num==-209) {
+        return $mensaje[EMAIL_INCORRECTO];
+    }elseif ($num==-210) {
+        return $mensaje[PASS_DIFERENTES];
+    }elseif ($num==-305) {
+        return $mensaje[PASS_INCORRECTO];
+    }  
         
 }
+/**
+ * Un numero de telefono es valido si tiene 9 digitos
+ * @param type $num
+ * @return boolean
+ */
 function esTelefono($num){
-    if(preg_match("/^[0-9]{9}/",$num)){
+    if(preg_match("/^[0-9]{9}$/",$num)){
         return true;
     }else{
         return false;
     }
 }
+/**
+ * Un nombre ó apellido es válido si tiene un mímimo de 3 caracteres y un máximo de 20
+ * Además, que no empiece por números,puede contener espacios en blanco y que no contenga caracteres especiales
+ * @param type $nombre 
+ * @return boolean true si se cumplen las reglas. False en caso contrario
+ */
+function esNombreValido($nombre) {
+    if (strlen($nombre) < 3 && strlen($nombre) > 20) {
+        return false;
+    } else {
+        //Que no empiece por números,puede contener espacios en blanco y que no contenga caracteres especiales
+        if (preg_match("/^[A-Z \-áéíóúÁÉÍÓÚñÑ\\s]+[^\*=;:+.]*/i", $nombre)) {
+            return true;
+        } else {
+           
+            return false;
+        }
+    }
+}
+/**
+ * Funcion a la que se le pasa un nombre simple ó compuesto y transforma la primera letra a Mayúsculas
+ * @param type $nombre
+ * @return string devuleve el nombre con la primera letra en mayúsculas
+ */
+function  ponerLetraEnMayuscula($nombre){
+     $nombreCompuesto=  explode(" ", $nombre);
+     $nombreConPrimeraletramayus="";
+     for ($i = 0; $i < count($nombreCompuesto); $i++) {
+       $letra= strtoupper(substr($nombreCompuesto[$i],0,1));
+      $nombreCompuesto[$i]=$letra.substr($nombreCompuesto[$i],1);
 
+     }
+     for ($i = 0; $i < count($nombreCompuesto); $i++) {
+          
+          $nombreConPrimeraletramayus.=$nombreCompuesto[$i]." ";
+     }
+     return $nombreConPrimeraletramayus;
+ }
+ /**
+  * Con un filtro validamos la direccion de correo electronico
+  * @param type $mail 
+  * @return boolean true si es valido. False en caso contrario
+  */
+ function esMailValido($mail){
+     if (filter_var($mail, FILTER_VALIDATE_EMAIL)) 
+         return true;
+     return false;
+ }
 ?>
